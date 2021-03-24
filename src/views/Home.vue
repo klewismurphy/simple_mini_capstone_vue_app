1a1dafc064018cfdf3e91e7644698216bedad777
Kaitlin <klewismurphy@gmail.com>
2021-03-23 21:27:14 -0500

    adds index and create actions for the frontend. tried my hardest to delete prettier but it holds on still

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,61 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h2> {{ message2 }} </h2>
-    <body>
-      <button v:on-click>Click away</button>
-    </body>
+    <div v-bind:key="product.id" v-for="product in products">
+    {{ product.name }}
+    </div>
+    <br><hr><br>
+    <p> Name: <input type="text" v-model="name"></p>
+    <p> Description: <input type="text" v-model="description"></p>
+    <p> Price: <input type="number" v-model="price"></p>
+    <p> Image Url: <input type="text" v-model="image_url"></p>
+    <button v-on:click="createProducts">Create a Product</button>
   </div>
 </template>
+
 <style></style>
+
+
+
 <script>
+import axios from "axios";
+
 export default {
-  data: function() {
+  data: function () {
     return {
       message: "HEY",
-      message2: "WELCOME"
+      products: [],
+      name: "",
+      description: "",
+      price: "",
+      image_url: "",
     };
   },
-  created: function() {},
+  created: function () {
+    this.indexProducts("success");
+  },
   methods: {
-
-  }
+    indexProducts: function () {
+      console.log("getting products...");
+      axios.get("http://localhost:3000/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+    createProducts: function () {
+      console.log("creating a product...");
+      axios
+        .post("http://localhost:3000/api/products", {
+          name: this.name,
+          description: this.description,
+          price: this.price,
+          image_url: this.image_url,
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.products.push(response.data);
+        });
+    },
+  },
 };
 </script>
